4fa0eb0cf8d0985a74005fdd8f69cd7461ebb316
James Burns <jrburns96@aol.com>
2022-01-18 11:33:59 -0500

    commit tree 9a2435693df3f02c32b15f94d4e7d2ec17daf46b

---
+++ app/serializers/author_post_serializer.rb
@@ -0,0 +1,11 @@
+class AuthorPostSerializer < ActiveModel::Serializer
+  attributes :title, :short_content
+
+  # belongs_to :author
+  has_many :tags
+
+  def short_content
+    # byebug
+    "#{self.object.content[0..39]}..."
+  end
+end
